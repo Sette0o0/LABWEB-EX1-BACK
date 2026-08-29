9c087a2743c1926d6a2cfe016d755dde32f01403
Rafael <rafaelsette12345@gmail.com>
2026-08-28 21:46:47 -0300

    feat: adiciona cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import fastify from "fastify"
+import cors from "@fastify/cors"
 import { controller } from "./controller.js"
 
 const app = fastify()
+
+await app.register(cors, {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+})
 
 app.register(controller)
 
